67158c76ac40da3b0f3b22d6d2a265f64a144288
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-09-04 20:31:58 +0200

    feat: operators - filter

--- src/operators/01-map.ts
+++ src/operators/01-map.ts
@@ -6,6 +6,11 @@ import { map } from 'rxjs/operators';
 //   .subscribe(console.log);
 
 const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup');
+
 const keyupCode$ = keyup$.pipe(map((event) => event.code));
 
+const keyupMapTo$ = keyup$.pipe(map(() => 'Press'));
+
+keyup$.subscribe(console.log);
 keyupCode$.subscribe((code) => console.log('map', code));
+keyupMapTo$.subscribe((val) => console.log(val));
